a0e72d0a0cc21f1b0d180f3272f362eaf1ed675c
Gustavo Melo <costa.2005@alunos.utfpr.edu.br>
2026-06-17 15:42:23 -0300

    criação dos elementos de busca + início do file .js

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputBusca = document.getElementById('input-busca');
+const btnBuscar = document.getElementById('btn-buscar');
+const containerResultados = document.getElementById('container-resultados');
+const containerFavoritos = document.getElementById('container-favoritos');
+const erroValidacao = document.getElementById('erro-validacao');
